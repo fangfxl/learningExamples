492a57a821b7191fcda5b92a70e90e0bab868615
fangxl <fangxl@minstone.com.cn>
2020-09-04 09:24:59 +0800

    提交

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,113 +1,73 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
-  </div>
+    <div id="index">
+      <ul>
+        <li v-for="(item, index) of imgArr" :key="index"> <img :src="item" alt="图片描述"></li>
+      </ul>
+      <div class="upload">
+        <input type="file" >
+      </div>
+    </div>
 </template>
 
 <script>
+import Viewer from 'viewerjs';
+import 'viewerjs/dist/viewer.css';
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+     imgArr:[
+       'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2785097329,1326408874&fm=26&gp=0.jpg',
+       'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1599127857085&di=b1279ffff5ad94528206d1f418a03750&imgtype=0&src=http%3A%2F%2Fdpic.tiankong.com%2F2f%2Fhm%2FQJ6348448923.jpg%3Fx-oss-process%3Dstyle%2Fshow',
+       'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2785097329,1326408874&fm=26&gp=0.jpg'
+     ],
     }
+  },
+  mounted() {
+    const ViewerDom = document.getElementById('index');
+    const viewer = new Viewer(ViewerDom, {
+    // 'zIndex': 9999,
+    // 'inline': true,
+    // 'button': true,
+    // 'navbar': true,
+    // 'title': true, 
+    // 'toolbar': true, 
+    // 'tooltip': true, 
+    // 'movable': true, 
+    // 'zoomable': true, 
+    // 'rotatable': true, 
+    // 'scalable': true, 
+    // 'transition': true, 
+    // 'fullscreen': true, 
+    // 'keyboard': true, 
+    // 'url': 'data-source'
+    })
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+*{
+padding:0;
+margin: 0;
 }
 ul {
-  list-style-type: none;
-  padding: 0;
+display: flex;
+flex-wrap: wrap;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+ul li{
+width:265px;
+height: 180px;
+list-style: none;
+border: 2px solid #CCC;
+border-radius: 3px;
+padding: 1px;
+margin: 10px;
+cursor: pointer;
 }
-a {
-  color: #42b983;
+ul li img{
+width: 100%;
+height: 100%;
 }
 </style>
